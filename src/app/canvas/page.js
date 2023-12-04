a914f9acc8b739b3edb1a8a5bf5423d61b087dc8
JMA CASA <lacasadelaarquitectura@hotmail.com>
2023-12-04 19:57:06 +0100

    v0.2

--- src/app/canvas/page.js
+++ src/app/canvas/page.js
@@ -4,8 +4,18 @@ import Card from "../ui/card";
 export default function Page() {
 
     return (
-      <article className="grid grid-cols-6">
-      <section className="col-span-3"><Card /></section>
+      <article className="flex  gap-6 flex-wrap m-5">
+        
+       
+      <Card titulo={'PF0'} path={'/boe3.png'} descripcion={'Normativa'}/>
+      <Card titulo={'PF01'} path={'/aislamiento.png'} descripcion={'Cerramientos'}/>
+      <Card titulo={'PF02'} path={'/aislamiento2.png'} descripcion={'Transmitancias'}/>
+      <Card titulo={'PF03'} path={'/aire.png'} descripcion={'Psicrometria'}/>
+      <Card titulo={'PF04'} path={'/radiador.png'} descripcion={'Q de Calefacción'}/>
+      <Card titulo={'PF05'} path={'/incendios.png'} descripcion={'Incendios'}/>
+      <Card titulo={'PF06'} path={'/neveras.png'} descripcion={'Ciclo Frigorífico'}/>
+      <Card titulo={'PF07'} path={'/camara.png'} descripcion={'Paneles Frigoríficos'}/>
+      <Card titulo={'PF08'} path={'/pinguinos.png'} descripcion={'Q de refrigeración'}/>
       </article>
     )};
   
--- src/app/ui/card.js
+++ src/app/ui/card.js
@@ -1,7 +1,12 @@
-export default function Card(){
+import Image from "next/image"
+
+export default function Card(props){
     return(
-        <div className="bg-black w-20 h-20">
-            
-        </div>
+        <article className="">
+
+        <Image className="rounded-xl h-28 w-28" src={props.path} width={125} height={125} ></Image>
+        <h4 className="m-1">{props.titulo}</h4>
+        <p className="text-xs font-light ml-1">{props.descripcion}</p>
+        </article>
     )
 }

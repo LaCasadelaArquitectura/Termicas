c8c44be58277456b8d159a345aef15aa2b0a77e3
La Casa de la Arquitectura <lacasadelaarquitectura@hotmail.com>
2023-12-15 19:41:32 +0100

    v0.9

--- Data/presentaciones.js
+++ Data/presentaciones.js
@@ -3,77 +3,77 @@ export default {
     "data": {
         "presentaciones": [
             {
-                "id": "0",
+                "id": "1",
                 "name": "PF00",
                 "path": "/boe3.png",
                 "description": "Normativa",
                 "url": "https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAEmVMx0RgI&#x2F;view?embed"
             },
             {
-                "id": "1",
+                "id": "2",
                 "name": "PF01",
                 "path": "/aislamiento.png",
                 "description": "Cerramientos",
                 "url": ""
             },
             {
-                "id": "2",
+                "id": "3",
                 "name": "PF02",
                 "path": "/aislamiento2.png",
                 "description": "Transmitancias",
                 "url": ""
             },
             {
-                "id": "3",
+                "id": "4",
                 "name": "PF03",
                 "path": "/aire.png",
                 "description": "Psicrometria",
                 "url": "https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAFQ_SWjfUk&#x2F;view?embed"
             },
             {
-                "id": "4",
+                "id": "5",
                 "name": "PF04",
                 "path": "/radiador.png",
                 "description": "Q de Calefacción",
                 "url": "https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAF0V6jGvFE&#x2F;view?embed"
             },
             {
-                "id": "5",
+                "id": "6",
                 "name": "PF05",
                 "path": "/tuberias.png",
                 "description": "Cálculo Tuberías",
                 "url": "https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAFhYbYHZ-4&#x2F;view?embed"
             },
             {
-                "id": "6",
+                "id": "7",
                 "name": "PF06",
                 "path": "/incendios.png",
                 "description": "Contra-incendios",
                 "url": ""
             },
             {
-                "id": "7",
+                "id": "8",
                 "name": "PF07",
                 "path": "/neveras.png",
                 "description": "Ciclo Frigorífico",
                 "url": "https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAFXK0KyMFE&#x2F;view?embed"
             },
             {
-                "id": "8",
+                "id": "9",
                 "name": "PF08",
                 "path": "/camara.png",
                 "description": "Paneles Frigoríficos",
                 "url": "https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAFS9xGlQWc&#x2F;view?embed"
             },
             {
-                "id": "9",
+                "id": "10",
                 "name": "PF09",
                 "path": "/pinguinos.png",
                 "description": "Q de refrigeración",
                 "url": "https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAFTPls51cc&#x2F;view?embed"
             },
             {
-                "id": "10",
+                "id": "11",
                 "name": "PF10",
                 "path": "/Chiller.jpg",
                 "description": "Componentes f",

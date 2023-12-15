2d884743a4e29fac9db87eba894476ba96f90451
La Casa de la Arquitectura <lacasadelaarquitectura@hotmail.com>
2023-12-15 19:14:25 +0100

    v0.9

--- ui/card.js
+++ ui/card.js
@@ -2,7 +2,7 @@ import Image from "next/image"
 
 export default function Card(props) {
     return (
-        <button onClick={props.handleClick} className="cursor-pointer z-10 hover:transform hover:translate-y-1 hover:-translate-x-1 hover:font-bold transition-all ease-in-out delay-50">
+        <button key={props.key} onClick={props.handleClick} className="cursor-pointer z-10 hover:transform hover:translate-y-1 hover:-translate-x-1 hover:font-bold transition-all ease-in-out delay-50">
 
             <Image className="rounded-xl h-28 w-28 hover:shadow-xl" src={props.path} width={125} height={125} ></Image>
             <h4 className="m-1 ">{props.name}</h4>
